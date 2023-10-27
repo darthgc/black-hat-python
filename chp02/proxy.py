557d7d533853dbde86f602d65116e1effef65fa6
darthgc <guillaume.croteau.1@gmail.com>
2023-10-27 15:44:41 -0400

    Working on proxy

---
+++ chp02/proxy.py
@@ -0,0 +1,50 @@
+import socket
+import sys
+import threading
+
+HEX_FILTER = "".join([(len(repr(chr(i))) == 3) and chr(i) or "." for i in range(256) ]) # Tranform to printable characters, otherwise put a .
+
+def hexdump(src, length=16, show=True):
+    if isinstance(src, bytes):
+        src = src.decode()
+    
+    results = list()
+
+    for i in range(0, len(src), length):
+        word = str(src[i:i+length])
+
+        printable = word.translate(HEX_FILTER)
+        hexa = " ".join([f"{ord(c):02X}" for c in word])
+        hexwidth = length * 3
+        results.append(f"{i:04x}  {hexa:<{hexwidth}}  {printable}")
+
+    if show:
+        for line in results:
+            print(line)
+    else:
+        return results
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
+
+def request_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+def response_handler(buffer):
+    # perform packet modifications
+    return buffer
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    # TODO
+    return
