54cf63fe4432526d9d65ad532dd74627216436b4
darthgc <guillaume.croteau.1@gmail.com>
2023-12-15 15:50:37 -0500

    Worked on bhp chapters 5,6,7.

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Source code related to the book Black Hat Python (Second Edition)
 
+Notes:
+
+- Chapter 6 is not present, because the book does not use the new Burp API. It would be best to use Java when extending Burp anyway.
+- Chapter 8, the keylogger is not present, because it uses a dead modules that does not work with an up to date version of Python.
+
 ## Setup
 
 - `python -m venv venv3`
@@ -9,7 +14,7 @@ Source code related to the book Black Hat Python (Second Edition)
 
 Once done: `deactivate`
 
-Dependencies:
+Python dependencies:
 
 - `pip install lxml`
 - `pip install beautifulsoup4`
@@ -17,5 +22,11 @@ Dependencies:
 - `pip install scapy`
 - `pip install opencv-python opencv-python-headless`
 - `pip install requests`
+- `pip install github3.py`
+- `pip install pyWinhook`
+
+OS dependencies:
 
 - `sudo apt-get install libopencv-dev python3-opencv python3-numpy python3-scipy`
+
+For Chapter 7, you need to create a GitHub PAT in order to communicate with the C2 and store it in a file named `mytoken.txt`
--- chp05/mapper.py
+++ chp05/mapper.py
@@ -16,13 +16,38 @@ web_paths = queue.Queue()
 def gather_paths():
     for root, _, files in os.walk("."):
         for fname in files:
-            if os.path.splittext(fname)[1] in FILTERED:
+            if os.path.splitext(fname)[1] in FILTERED:
                 continue
             path = os.path.join(root, fname)
             if path.startswith("."):
                 path = path[1:]
             print(path)
             web_paths.put(path)
+
+def test_remote():
+    while not web_paths.empty():
+        path = web_paths.get()
+        url = f"{TARGET}{path}"
+        time.sleep(2) # Your target may have throttling/lockout
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            answers.put(url)
+            sys.stdout.write("+")
+        else:
+            sys.stdout.write("x")
+        sys.stdout.flush()
+
+def run():
+    mythreads = list()
+    for i in range(THREADS):
+        print(f"Spawning thread {i}")
+        t = threading.Thread(target=test_remote)
+        mythreads.append(t)
+        t.start()
+    
+    for thread in mythreads:
+        thread.join()
 
 @contextlib.contextmanager
 def chdir(path):
@@ -42,3 +67,9 @@ if __name__ == "__main__":
     with chdir("/home/kali/Downloads/wordpress"):
         gather_paths()
     input("Press return to continue.")
+
+    run()
+    with open("myanswers.txt", "w") as f:
+        while not answers.empty():
+            f.write(f"{answers.get()}\n")
+    print("Done")
